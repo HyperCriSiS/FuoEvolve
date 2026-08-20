3681480f9c8040236e75ad4755881aef1c957569
Bruce Zhang <zttt183525594@gmail.com>
2026-08-20 21:55:13 +0800

    fix: restore playlist state delegates

--- shared/src/commonMain/kotlin/org/feeluown/mobile/feature/localplaylist/PlaylistControllerState.kt
+++ shared/src/commonMain/kotlin/org/feeluown/mobile/feature/localplaylist/PlaylistControllerState.kt
@@ -1,6 +1,8 @@
 package org.feeluown.mobile
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
